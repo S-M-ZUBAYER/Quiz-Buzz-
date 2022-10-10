fe53d4e48b0626f6bb62a02f9a9c32a2528bac50
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-10-11 04:09:38 +0600

    Footer section added

--- src/components/Home.js/Home.js
+++ src/components/Home.js/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import img from '../../img/heading.jpg'
+import Heading from '../Heading/Heading';
 import ItemCart from '../ItemCart/ItemCart';
 
 const Home = () => {
@@ -8,55 +8,17 @@ const Home = () => {
     console.log(courses.data)
     return (
         <div>
-            <section className="dark:dark:bg-gray-800 dark:dark:text-gray-100">
-                <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                    <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-                        <h1 className="text-5xl font-bold leading-none sm:text-6xl">Ac mattis
-                            <span className="dark:dark:text-violet-400">senectus</span>erat pharetra
-                        </h1>
-                        <p className="mt-6 mb-8 text-lg sm:mb-12">Dictum aliquam porta in condimentum ac integer
-                            <br className="hidden md:inline lg:hidden" />turpis pulvinar, est scelerisque ligula sem
-                        </p>
-                        <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold rounded dark:dark:bg-violet-400 dark:dark:text-gray-900">Suspendisse</a>
-                            <a rel="noopener noreferrer" href="#" className="px-8 py-3 text-lg font-semibold border rounded dark:dark:border-gray-100">Malesuada</a>
-                        </div>
-                    </div>
-                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-                        <img src={img} alt="" className="object-contain h-72 sm:h-80 lg:h-96 w-96 xl:h-112 2xl:h-128" />
-                    </div>
-                </div>
-            </section>
+            <Heading></Heading>
             <section className="my-8 dark:dark:bg-gray-800 dark:dark:text-gray-100">
                 <div className="container flex flex-col items-center mx-auto mb-12 md:p-10 md:px-12">
                     <h1 className="p-4 text-4xl font-semibold leading-none text-center">What our customers are saying about us</h1>
                 </div>
-                <div className="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10">
-                    <div className="flex flex-col max-w-sm mx-4 my-6 shadow-lg bg-slate-700">
-                        <div className="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:dark:bg-gray-900">
-                            <p className="relative px-6 py-1 text-lg italic text-center dark:dark:text-gray-100">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="w-8 h-8 dark:dark:text-violet-400">
-                                    <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
-                                    <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
-                                </svg>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus quibusdam, eligendi exercitationem molestias possimus facere.
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute right-0 w-8 h-8 dark:dark:text-violet-400">
-                                    <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
-                                    <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
-                                </svg>
-                            </p>
-                        </div>
-                        <div className="flex flex-col items-center justify-center p-8 rounded-b-lg dark:dark:bg-violet-400 dark:dark:text-gray-900">
-                            <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:dark:bg-gray-500 dark:dark:bg-gray-700" />
-                            <p className="text-xl font-semibold leading-tight">Distinctio Animi</p>
-                            <p className="text-sm uppercase">Aliquam illum</p>
-                        </div>
-                    </div>
-
-
-
-                </div>
             </section>
-            <ItemCart></ItemCart>
+            <div className="max-w-screen-xl p-5 mx-auto dark:dark:bg-gray-800 dark:dark:text-gray-100">
+                <div className="grid grid-cols-1 gap-5 lg:grid-cols-4 sm:grid-cols-2">
+                    {(courses.data).map(topic => <ItemCart key={topic.id} topic={topic}></ItemCart>)}
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
-const ItemCart = () => {
-    return (
-        <div>
-            <h1>hojsfjd</h1>
+const ItemCart = ({ topic }) => {
+    const { name, logo, total } = topic;
 
+    return (
+
+        <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover h-96 bg-slate-600" style={{ backgroundImage: `url(${logo})` }}>
+            <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-b dark:dark:via-transparent dark:dark:from-gray-900 dark:dark:to-gray-900"></div>
+            <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
+                <p className="px-3 py-2 text-lg font-bold tracking-wider uppercase dark:dark:text-gray-100">{name}</p>
+                <div className="flex flex-col justify-start text-center dark:dark:text-gray-100">
+                    <button className="btn btn-info">Start</button>
+
+                </div>
+            </div>
+            <h2 className="z-10 p-5">
+                <p className="font-bold text-lg dark:dark:text-gray-100">Total Quiz: {total}</p>
+            </h2>
         </div>
+
+
     );
 };
 
